89db2f3a738716478a63686419514ee86b1d480b
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-10-31 17:52:35 +0300

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let fib = calculate_fibonacci(23);
+    println!("{}", fib);
+}
+
+fn calculate_fibonacci(n: i32) -> i32 {
+    let mut fib1 = 1;
+    let mut fib2 = 1;
+
+    let mut i = 0;
+
+    while i < n - 2 {
+        let fib_sum = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib_sum;
+        i += 1;
+    };
+    fib2
 }
